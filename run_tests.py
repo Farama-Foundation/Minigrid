de3f757e3e4bfffb98b322ff78a3865e13660c90
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2017-12-16 20:41:02 -0500

    Added to run_tests.sh

--- run_tests.py
+++ run_tests.py
@@ -7,15 +7,20 @@ from gym_minigrid.register import envSet
 print('%d environments registered' % len(envSet))
 
 for envName in sorted(envSet):
-    #print(envName)
+    print('testing "%s"' % envName)
 
     # Load the gym environment
     env = gym.make(envName)
     env.reset()
     env.render('rgb_array')
 
+    env.seed()
+    env.reset()
+
     numActions = env.action_space.n
     for i in range(500):
         action = random.randint(0, numActions - 1)
         env.step(action)
         env.render('rgb_array')
+
+    env.close()
