3a7ee006abc137838ce4a50217276d0b9d503bb1
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2018-03-15 16:44:21 +0100

    changed run scripts to multiroom environment

--- rl_ex_results.sh
+++ rl_ex_results.sh
@@ -3,4 +3,4 @@ PYTHONPATH=./gym_minigrid/:$PYTHONPATH
 PYTHONPATH=./gym_minigrid/envs/:$PYTHONPATH
 export PYTHONPATH
 
-python3 pytorch_rl/enjoy.py --env-name MiniGrid-Empty-6x6-v0 --load-dir ./trained_models/a2c
+python3 pytorch_rl/enjoy.py --env-name MiniGrid-MultiRoom-N6-v0 --load-dir ./trained_models/a2c
--- rl_ex_run.sh
+++ rl_ex_run.sh
@@ -3,4 +3,4 @@ PYTHONPATH=./gym_minigrid/:$PYTHONPATH
 PYTHONPATH=./gym_minigrid/envs/:$PYTHONPATH
 export PYTHONPATH
 
-python3 pytorch_rl/main.py --env-name MiniGrid-Empty-6x6-v0 --no-vis --num-processes 48 --algo a2c
+python3 pytorch_rl/main.py --env-name MiniGrid-MultiRoom-N6-v0 --no-vis --num-processes 48 --algo a2c
