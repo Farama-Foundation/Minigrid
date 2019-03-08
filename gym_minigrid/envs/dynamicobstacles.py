c68875ff77dc31fb6d6c776db215f0df2e9a04e7
Suman Pal <suman7495@gmail.com>
2019-03-08 17:14:05 +0100

    Added new environment: Dynamic Obstacles

---
+++ gym_minigrid/envs/dynamicobstacles.py
@@ -0,0 +1,98 @@
+from gym_minigrid.minigrid import *
+from gym_minigrid.register import register
+
+class DynamicObstaclesEnv(EmptyEnv):
+    """
+    Empty grid environment with moving obstacles
+    """
+
+    def __init__(
+            self,
+            size=8,
+            agent_start_pos=(1, 1),
+            agent_start_dir=0,
+    ):
+        self.agent_start_pos = agent_start_pos
+        self.agent_start_dir = agent_start_dir
+
+        super().__init__(
+            grid_size=size,
+            max_steps=4 * size * size,
+            # Set this to True for maximum speed
+            see_through_walls=True
+        )
+
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Place a goal square in the bottom-right corner
+        self.grid.set(width - 2, height - 2, Goal())
+
+        # Place the agent
+        if self.agent_start_pos is not None:
+            self.start_pos = self.agent_start_pos
+            self.start_dir = self.agent_start_dir
+        else:
+            self.place_agent()
+
+        self.mission = "get to the green goal square"
+
+
+class DynamicObstaclesEnv5x5(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=5)
+
+
+class DynamicObstaclesRandomEnv5x5(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=5, agent_start_pos=None)
+
+
+class DynamicObstaclesEnv6x6(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=6)
+
+
+class DynamicObstaclesRandomEnv6x6(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=6, agent_start_pos=None)
+
+
+class DynamicObstaclesEnv16x16(EmptyEnv):
+    def __init__(self):
+        super().__init__(size=16)
+
+
+register(
+    id='MiniGrid-Dynamic-Obstacles-5x5-v0',
+    entry_point='gym_minigrid.envs:DynamicObstaclesEnv5x5'
+)
+
+register(
+    id='MiniGrid-Dynamic-Obstacles-Random-5x5-v0',
+    entry_point='gym_minigrid.envs:DynamicObstaclesRandomEnv5x5'
+)
+
+register(
+    id='MiniGrid-Dynamic-Obstacles-6x6-v0',
+    entry_point='gym_minigrid.envs:DynamicObstaclesEnv6x6'
+)
+
+register(
+    id='MiniGrid-Dynamic-Obstacles-Random-6x6-v0',
+    entry_point='gym_minigrid.envs:DynamicObstaclesRandomEnv6x6'
+)
+
+register(
+    id='MiniGrid-Dynamic-Obstacles-8x8-v0',
+    entry_point='gym_minigrid.envs:DynamicObstaclesEnv'
+)
+
+register(
+    id='MiniGrid-Dynamic-Obstacles-16x16-v0',
+    entry_point='gym_minigrid.envs:DynamicObstaclesEnv16x16'
+)
--- manual_control.py
+++ manual_control.py
@@ -17,7 +17,7 @@ def main():
         "--env-name",
         dest="env_name",
         help="gym environment to load",
-        default='MiniGrid-MultiRoom-N6-v0'
+        default='MiniGrid-Dynamic-Obstacles-8x8-v0'
     )
     (options, args) = parser.parse_args()
 
