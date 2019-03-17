6f94b74af0b9748c390c5c63e8e447732f7743fb
Suman Pal <suman7495@gmail.com>
2019-03-17 14:00:29 +0100

    Updated Dynamic Obstacles environment

--- gym_minigrid/envs/__init__.py
+++ gym_minigrid/envs/__init__.py
@@ -1,4 +1,3 @@
-from gym_minigrid.envs.dynamicobstacles import *
 from gym_minigrid.envs.empty import *
 from gym_minigrid.envs.doorkey import *
 from gym_minigrid.envs.multiroom import *
@@ -17,3 +16,4 @@ from gym_minigrid.envs.obstructedmaze import *
 from gym_minigrid.envs.memory import *
 from gym_minigrid.envs.fourrooms import *
 from gym_minigrid.envs.crossing import *
+from gym_minigrid.envs.dynamicobstacles import *
