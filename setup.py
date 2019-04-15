35d72af01c219c50afba9f39d22ab0964484ac52
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2019-04-15 16:55:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gym_minigrid',
-    version='0.0.1',
+    version='0.0.2',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
     packages=['gym_minigrid', 'gym_minigrid.envs'],
     install_requires=[
