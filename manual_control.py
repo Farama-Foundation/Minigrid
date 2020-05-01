ba1f48a723613c0d41bde2cbb3589dfa012f10f5
Akriti Anand <akriti.anand15@gmail.com>
2020-05-01 19:41:41 -0400

    phase 1 with rdf tuples

--- manual_control.py
+++ manual_control.py
@@ -7,6 +7,8 @@ import gym
 import gym_minigrid
 from gym_minigrid.wrappers import *
 from gym_minigrid.window import Window
+import rdflib
+from rdflib import Graph
 
 def redraw(img):
     if not args.agent_view:
@@ -26,15 +28,130 @@ def reset():
 
     redraw(obs)
 
+
+
 def step(action):
     obs, reward, done, info = env.step(action)
     print('step=%s, reward=%.2f' % (env.step_count, reward))
-
+    o = obs["image"]
+    o = o.transpose()
+    print(o)
+    
+    o = preprocess(o)
+    rdf(o)
+    
     if done:
         print('done!')
         reset()
     else:
         redraw(obs)
+
+# Map of object type to integers old
+OBJECT_TO_IDX_OLD = {
+    'unseen'        : 0,
+    'empty'         : 1,
+    'wall'          : 2,
+    'floor'         : 3,
+    'door'          : 4,
+    'key'           : 5,
+    'ball'          : 6,
+    'box'           : 7,
+    'goal'          : 8,
+    'lava'          : 9,
+    'agent'         : 10
+}
+
+# Map of object type to integers new
+OBJECT_TO_IDX_NEW = {
+    'agent'         : 0,
+    'key'           : 1,
+    'door'          : 2,
+    'goal'          : 3
+}
+
+RELATION = {
+    'visible'   : 0,
+    'pciked'    : 1,
+    'locked'    : 2
+}
+
+def preprocess(old_state):
+    dim = len(OBJECT_TO_IDX_NEW)
+    old_key_list = list(OBJECT_TO_IDX_OLD.keys())
+    old_val_list = list(OBJECT_TO_IDX_OLD.values()) 
+    obs = (3,dim,dim)
+    obs=np.zeros(obs, dtype=int)
+
+    visible = (dim,dim)
+    visible=np.zeros(visible, dtype=int)
+
+    carrying = (dim,dim)
+    carrying=np.zeros(carrying, dtype=int)
+
+    locked = (dim,dim)
+    locked=np.zeros(locked, dtype=int)
+
+    for key in OBJECT_TO_IDX_NEW:
+        old_index = OBJECT_TO_IDX_OLD[key]
+        new_index = OBJECT_TO_IDX_NEW[key]
+        found = np.where(old_state[0] == old_index)
+        print("agent ",old_state[0][6][3])
+        print("key ",old_index)
+
+        if(found[0].size>0):
+            visible[0][new_index] = 1
+
+    obs[0] = visible
+    
+    if(old_state[0][6][3]!=1):
+        carrying_object = old_key_list[old_val_list.index(old_state[0][6][3])]
+        new_index = OBJECT_TO_IDX_NEW[carrying_object]
+        carrying[0][new_index] = 1
+
+    obs[1] = carrying
+
+    is_door_locked = np.where(old_state[2] == 2)
+    door_index = OBJECT_TO_IDX_NEW['door']
+    if(is_door_locked[0].size>0):
+        locked[0][door_index] = 1
+
+    obs[2] = locked
+        
+    #print(obs)
+    return obs
+    
+def rdf(o):
+    state=[]
+
+    visible = o[0]
+    carrying = o[1]
+    locked = o[2] 
+
+    objects_visible = np.where(visible[0] == 1)
+    objects_carrying = np.where(carrying[0] == 1)
+    door_locked =  np.where(locked[0] == 1)
+
+    key_list = list(OBJECT_TO_IDX_NEW.keys()) 
+    val_list = list(OBJECT_TO_IDX_NEW.values()) 
+
+    print(locked)
+
+    for b in objects_visible[0]:
+        object = key_list[val_list[b]]
+        state.append(("agent", "visible", object))
+
+    for b in objects_carrying[0]:
+        object = key_list[val_list[b]]
+        state.append(("agent", "carrying", object))
+
+    for b in door_locked[0]:
+        object = key_list[val_list[b]]
+        state.append(("agent", "locked", object))
+
+    for s, p, o in state:
+        print((s, p, o))
+    
+    
 
 def key_handler(event):
     print('pressed', event.key)
