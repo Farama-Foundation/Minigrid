3c55de6e82ca0bba9fe871886bb806b2d5fd7341
Henrik Mettler <henrik.mettler@gmail.com>
2021-07-23 15:33:12 +0200

    Example Env with one Sand element

---
+++ gym_minigrid/envs/one_sand_element.py
@@ -0,0 +1,45 @@
+from gym_minigrid.minigrid import *
+from gym_minigrid.register import register
+
+class OneSandElementEnv(MiniGridEnv):
+    """
+    Empty grid environment, no obstacles except 1 element of sand, sparse reward
+    """
+
+    def __init__(
+        self,
+        size=8,
+        agent_start_pos=(1,1),
+        agent_start_dir=0,
+    ):
+        self.agent_start_pos = agent_start_pos
+        self.agent_start_dir = agent_start_dir
+
+        super().__init__(
+            grid_size=size,
+            max_steps=4*size*size,
+            # Set this to True for maximum speed
+            see_through_walls=True
+        )
+
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Place a goal square in the bottom-right corner
+        self.put_obj(Goal(), width - 2, height - 2)
+
+        # Place one a element on Sand in the middle
+        self.put_obj(Sand(), width//2, height//2)
+
+        # Place the agent
+        if self.agent_start_pos is not None:
+            self.agent_pos = self.agent_start_pos
+            self.agent_dir = self.agent_start_dir
+        else:
+            self.place_agent()
+
+        self.mission = "get to the green goal square"
