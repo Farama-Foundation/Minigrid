32c30862b0a547e6464d2a4b249bd175d1070034
Henrik Mettler <henrik.mettler@gmail.com>
2021-07-23 15:35:25 +0200

    Add OneSandElementEnv to init

--- gym_minigrid/envs/__init__.py
+++ gym_minigrid/envs/__init__.py
@@ -20,3 +20,5 @@ from gym_minigrid.envs.lavagap import *
 from gym_minigrid.envs.dynamicobstacles import *
 from gym_minigrid.envs.distshift import *
 from gym_minigrid.envs.dynamic_minigrid import *
+from gym_minigrid.envs.one_sand_element import *
+
