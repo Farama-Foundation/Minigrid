1d74747030a953e9e60cd09fb3d59b078aa351da
Ronnypetson Souza da Silva <rsronnypetson4@gmail.com>
2021-10-11 15:27:40 -0300

    checking default states with default dictionaries

--- project_RL/sarsa/sarsa_lambda_agent.py
+++ project_RL/sarsa/sarsa_lambda_agent.py
@@ -1,6 +1,6 @@
 import random
-
 import numpy as np
+from collections import defaultdict as dd
 
 
 class SarsaLambda:
@@ -24,33 +24,80 @@ class SarsaLambda:
         """Initialise q value table with zeros.
         Its first dimension is the state size and the second dimension is the action size.
         """
-        self.q_value_table = np.zeros((self.state_size, self.action_size), np.int)
+        # self.q_value_table = np.zeros((self.state_size, self.action_size), np.int)
+        self.q_value_table = dd(lambda: dd(float))
 
     def init_eligibility_table(self):
         """Initialise eligibility trace table with zeros. Must be invoked before each episode.
         Its first dimension is the state size and the second dimension is the action size.
         """
-        self.eligibility_table = np.zeros((self.state_size, self.action_size))
+        # self.eligibility_table = np.zeros((self.state_size, self.action_size))
+        self.eligibility_table = dd(lambda: dd(float))
 
     def get_new_action_e_greedly(self, state):
         """With probability 1 - epsilon choose the greedy action.
         With probability epsilon choose random action.
         """
-        q_state = self.q_value_table[state["direction"]]
+        # q_state = self.q_value_table[state["direction"]]
+        q_state = self.q_value_table[tuple(state)]
         if random.random() < self.epsilon:
             return random.choice(range(self.action_size))
         else:
-            return np.argmax(q_state)
+            max_action = 0
+            max_val = -float('inf')
+            for action in q_state.keys():
+                if q_state[action] > max_val:
+                    max_val = q_state[action]
+                    max_action = action
+            return max_action
+
+    def get_new_action_greedly(self, state):
+        """With probability 1.0 choose the greedy action.
+        With probability 0.0 choose random action.
+        """
+        q_state = self.q_value_table[tuple(state)]
+        max_action = 0
+        max_val = -float('inf')
+        for action in q_state.keys():
+            if q_state[action] > max_val:
+                max_val = q_state[action]
+                max_action = action
+        return max_action
+
+    # def _get_goal_position(self, state):
+    #     grid = state['image']
+    #     rows = len(grid)
+    #     columns = len(grid[0])
+    #     for row in range(rows):
+    #         for column in range(columns):
+    #             if grid[row][column][0] == 8:
+    #                 return 10 * row + column
+    #     return 0
 
     def update(self, state, action, reward, new_state, new_action, done):
         """Updates the state action value for every pair state and action
         in proportion to TD-error and eligibility trace
 
         """
-        q_value_state_s = self.q_value_table[state["direction"]]
-        q_value_new_state = self.q_value_table[new_state["direction"]]
+        # import pdb; pdb.set_trace()
+        # q_value_state_s = self.q_value_table[tuple(state["direction"])]
+        # q_value_new_state = self.q_value_table[tuple(new_state["direction"])]
+        _state = tuple(state)
+        _new_state = tuple(new_state)
+        q_value_state_s = self.q_value_table[_state]
+        q_value_new_state = self.q_value_table[_new_state]
         td_error = reward + self.discount_rate * q_value_new_state[new_action] - q_value_state_s[action]
 
+        # self.eligibility_table[state["direction"]][action] += 1.0
+        self.eligibility_table[_state][action] += 1.0
+        if np.abs(td_error) > 1e-4:
+            for state in self.q_value_table.keys():
+                for action in self.q_value_table[state].keys():
+                    # self.q_value_table[state][action] += self.learning_rate * td_error * self.eligibility_table[state][action]
+                    # self.eligibility_table[state][action] = self.discount_rate * 0.9 * self.eligibility_table[state][action]
+                    _state = tuple(state)
+                    self.q_value_table[_state][action] += self.learning_rate * td_error * self.eligibility_table[_state][action]
+                    self.eligibility_table[_state][action] = self.discount_rate * 0.9 * self.eligibility_table[_state][action]
         # TODO: update tables
         # for all state s and action a,
         # update q_value_table , if td_error != 0:
--- project_RL/sarsa/train.py
+++ project_RL/sarsa/train.py
@@ -1,8 +1,9 @@
 from project_RL.sarsa.sarsa_lambda_agent import SarsaLambda
 from gym_minigrid.wrappers import *
+from time import time
 
 
-def train(hyperparameters):
+def train(env, hyperparameters):
     """ Train a sarsa lambda agent in the requested environment
 
     Arguments:
@@ -12,15 +13,15 @@ def train(hyperparameters):
             - learning_rate
             - epsilon
     """
-    env = gym.make(hyperparameters['env_name'])
     agent = SarsaLambda(env, hyperparameters['discount_rate'],
                         hyperparameters['learning_rate'], hyperparameters['epsilon'])
 
     # initialise variables for plotting purpose
     step = 0
     total_reward = 0.0
+    prev_total_reward = 0.0
 
-    for episode in range(100):
+    for episode in range(int(1e4)):
         # reset environment before each episode
         agent.init_eligibility_table()
         observation = env.reset()
@@ -28,24 +29,49 @@ def train(hyperparameters):
         action = agent.get_new_action_e_greedly(state)
         done = False
 
-        env.render()
+        # env.render()
         while not done:
             observation, reward, done, info = env.step(action)
             next_state = observation  # TODO: change it after decision regarding state
             total_reward += reward
             next_action = agent.get_new_action_e_greedly(next_state)
 
+            # t0 = time()
             agent.update(state, action, reward, next_state, next_action, done)
+            # print(f'{time() - t0} elapsed.')
 
             state = next_state
             action = next_action
-            env.render()
-            print("a:", action, "i:", step, "reward:", reward, "info:", info)
+            # env.render()
+            # print("a:", action, "i:", step, "reward:", reward, "info:", info)
             if done:
-                print("done?", done, "total reward:", total_reward, "info:", info)
+                # env.render()
+                if total_reward > prev_total_reward:
+                    print("done?", done, "total reward:", total_reward, "info:", info)
+                    play(env, agent)
+                    prev_total_reward = total_reward
             step += 1
-
     env.close()
+    return agent
+
+
+def play(env, agent, episodes=1):
+    for episode in range(episodes):
+        # reset environment before each episode
+        observation = env.reset()
+        state = observation  # TODO: change it after decision regarding state
+        action = agent.get_new_action_e_greedly(state)
+        done = False
+        total_reward = 0
+
+        env.render()
+        while not done:
+            observation, reward, done, info = env.step(action)
+            env.render()
+            next_state = observation  # TODO: change it after decision regarding state
+            total_reward += reward
+            action = agent.get_new_action_e_greedly(next_state)
+        print(f'Total reward: {total_reward}')
 
 
 if __name__ == '__main__':
@@ -56,4 +82,5 @@ if __name__ == '__main__':
         'epsilon': 0.3
     }
 
-    train(hyperparameters)
+    env = gym.make(hyperparameters['env_name'])
+    agent = train(env, hyperparameters)
