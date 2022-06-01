4c110a445daba77ac34519c5ac4a0d21a3bbf5ab
Jordan Terry <jkterry0@gmail.com>
2022-06-01 00:41:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
 from setuptools import setup
 
+with open("README.md") as fh:
+    long_description = ""
+    header_count = 0
+    for line in fh:
+        if line.startswith("##"):
+            header_count += 1
+        if header_count < 2:
+            long_description += line
+        else:
+            break
+
 setup(
     name='gym_minigrid',
+    author="Farama Foundation",
+    author_email="jkterry@farama.org",
     version='1.0.2',
-    keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
-    url='https://github.com/maximecb/gym-minigrid',
-    description='Minimalistic gridworld package for OpenAI Gym',
+    keywords='memory, environment, agent, rl, gym',
+    url='https://github.com/Farama-Foundation/gym-minigrid',
+    description='Minimalistic gridworld reinforcement learning environments',
     packages=['gym_minigrid', 'gym_minigrid.envs'],
+    long_description=long_description,
+    python_requires=">=3.7, <3.11",
+    long_description_content_type="text/markdown",
     install_requires=[
-        'gym>=0.9.6',
-        'numpy>=1.15.0'
+        'gym>=0.24.0',
+        "numpy>=1.18.0"
     ]
+    classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+],
 )
