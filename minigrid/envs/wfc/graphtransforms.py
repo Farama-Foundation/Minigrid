3d27d41afb39ac69fdaa0bc8d65163c997b48136
Samuel Garcin <garcin.samuel@gmail.com>
2022-08-24 20:00:04 +0100

    fix: more logging, moving arrays to cpu at the start of check_validity_start_goal()

--- minigrid/envs/wfc/graphtransforms.py
+++ minigrid/envs/wfc/graphtransforms.py
@@ -329,8 +329,10 @@ class Nav2DTransforms:
         logger.info(f"encode_decoder_output_to_graph(): Executed goal_nodes = mode_Fx[..., goal_dim].argmax(dim=-1).")
         is_valid, mode_A = Nav2DTransforms.check_validity(mode_A, start_nodes, goal_nodes, n_nodes, correct_A=correct_A)
         logger.info(f"encode_decoder_output_to_graph(): Executed Nav2DTransforms.check_validity().")
-        #DEBUG: CRASHING HERE
-        adj = Nav2DTransforms.encode_reduced_adj_to_adj(mode_A.cpu().numpy())
+        #DEBUG: CRASHING HERE *2
+        logger.info(f"encode_decoder_output_to_graph(): Encoding mode_A to numpy.")
+        mode_A = mode_A.cpu().numpy()
+        adj = Nav2DTransforms.encode_reduced_adj_to_adj(mode_A)
         logger.info(f"encode_decoder_output_to_graph(): Nav2DTransforms.encode_reduced_adj_to_adj().")
         mode_Fx = mode_Fx.cpu()
 
@@ -593,6 +595,14 @@ class Nav2DTransforms:
         """
         Check if the start and goal nodes are valid.
         """
+        logger.info("check_validity_start_goal(): Entering function")
+        logger.info(f"check_validity_start_goal(): Input shapes: start_nodes {start_nodes.shape}, goal_nodes {goal_nodes.shape}, A_red {A_red.shape}")
+        device = start_nodes.device
+        start_nodes = start_nodes.cpu()
+        goal_nodes = goal_nodes.cpu()
+        A_red = A_red.cpu()
+
+        logger.info("check_validity_start_goal(): Moved, start_nodes, goal_nodes, A_red to cpu")
 
         batch_inds = torch.arange(0, start_nodes.shape[0])
 
@@ -601,12 +611,19 @@ class Nav2DTransforms:
         mask3 = A_red[batch_inds, goal_nodes].amax(dim=-1) < threshold
         # valid only if NOT(start==goal OR no edges from start OR no edges from goal)
         valid = ~(mask1 | mask2 | mask3)
-        # TODO: check with this
-        # check that start and goal are not isolated (inactive) nodes
-        # if start in list(nx.isolates(graph)) or goal in list(nx.isolates(graph)):
-        # return False
 
-        return valid
+        # start_nodes = start_nodes.cpu().numpy()
+        # #rewriting in numpy:
+        # batch_inds = np.arange(0, start_nodes.shape[0])
+        #
+        # mask1 = start_nodes == goal_nodes
+        # mask2 = A_red[batch_inds, start_nodes].max(axis=-1) < threshold
+        # mask3 = A_red[batch_inds, goal_nodes].max(axis=-1) < threshold
+        # # valid only if NOT(start==goal OR no edges from start OR no edges from goal)
+        # valid = ~(mask1 | mask2 | mask3)
+
+
+        return valid.to(device)
 
     @staticmethod
     def check_invalid_edges_reduced_A(A, n_nodes, correct_A=False, threshold=0.5):
@@ -648,16 +665,18 @@ class Nav2DTransforms:
         """
         Check if the reduced adjacency matrix is valid and checks if the start and goal nodes are placed correctly
         """
+        logger.debug(f"check_validity(): DOES DEBUG WORK HERE??")
         logger.info(f"check_validity(): Entered function.")
-        logger.info(f"check_validity(): Inputs:")
-        logger.info(f"A_red:{A_red}")
-        logger.info(f"start_nodes{start_nodes}")
-        logger.info(f"goal_nodes{goal_nodes}")
+        logger.info(f"check_validity(): Input shapes:")
+        logger.info(f"A_red:{A_red.shape}")
+        logger.info(f"start_nodes{start_nodes.shape}")
+        logger.info(f"goal_nodes{goal_nodes.shape}")
         logger.info(f"n_nodes={n_nodes}")
         logger.info(f"correct_A={correct_A}")
         logger.info(f"threshold={threshold}")
         A_red, valid_A = Nav2DTransforms.check_invalid_edges_reduced_A(A_red, n_nodes, correct_A, threshold=threshold)
         logger.info(f"check_validity(): Executed check_invalid_edges_reduced_A().")
+        # DEBUG: crash here
         valid_start_goal = Nav2DTransforms.check_validity_start_goal(start_nodes, goal_nodes, A_red, threshold=threshold)
         logger.info(f"check_validity(): Executed check_validity_start_goal().")
         valid = valid_A & valid_start_goal
