56f6a574540041d2f72c6bae0d5f791396e79d19
Rodrigo de Lazcano <r.l.p.v96@gmail.com>
2022-09-28 09:34:10 -0400

    Rename minigrid.py to minigrid_env.py (#249)

--- minigrid/__init__.py
+++ minigrid/__init__.py
@@ -1,6 +1,6 @@
 from gymnasium.envs.registration import register
 
-from minigrid import minigrid, wrappers
+from minigrid import minigrid_env, wrappers
 from minigrid.core import roomgrid
 from minigrid.core.world_object import Wall
 
--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,8 +1,8 @@
 import gymnasium as gym
 import numpy as np
 
-from minigrid.examples.benchmark import benchmark
-from minigrid.examples.manual_control import key_handler, reset
+from minigrid.benchmark import benchmark
+from minigrid.manual_control import key_handler, reset
 from minigrid.utils.window import Window
 
 
