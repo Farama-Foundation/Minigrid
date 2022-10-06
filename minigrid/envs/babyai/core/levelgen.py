b2e382a145808e9379066bcfb054c2d784f425b6
saleml <salemlahlou9@gmail.com>
2022-10-06 12:19:09 -0400

    Add BabyAI levels (#253)
    Co-authored-by: Mark Towers <mark.m.towers@gmail.com>

---
+++ minigrid/envs/babyai/core/levelgen.py
@@ -0,0 +1,208 @@
+"""
+Copied and adapted from https://github.com/mila-iqia/babyai
+"""
+from minigrid.core.constants import COLOR_NAMES
+from minigrid.core.roomgrid import Room
+from minigrid.envs.babyai.core.roomgrid_level import RoomGridLevel
+from minigrid.envs.babyai.core.verifier import (
+    LOC_NAMES,
+    OBJ_TYPES,
+    OBJ_TYPES_NOT_DOOR,
+    AfterInstr,
+    AndInstr,
+    BeforeInstr,
+    GoToInstr,
+    ObjDesc,
+    OpenInstr,
+    PickupInstr,
+    PutNextInstr,
+)
+
+
+class LevelGen(RoomGridLevel):
+    """
+    Level generator which attempts to produce every possible sentence in
+    the baby language as an instruction.
+    """
+
+    def __init__(
+        self,
+        room_size=8,
+        num_rows=3,
+        num_cols=3,
+        num_dists=18,
+        locked_room_prob=0.5,
+        locations=True,
+        unblocking=True,
+        implicit_unlock=True,
+        action_kinds=["goto", "pickup", "open", "putnext"],
+        instr_kinds=["action", "and", "seq"],
+        **kwargs
+    ):
+        self.num_dists = num_dists
+        self.locked_room_prob = locked_room_prob
+        self.locations = locations
+        self.unblocking = unblocking
+        self.implicit_unlock = implicit_unlock
+        self.action_kinds = action_kinds
+        self.instr_kinds = instr_kinds
+
+        self.locked_room = None
+
+        super().__init__(
+            room_size=room_size, num_rows=num_rows, num_cols=num_cols, **kwargs
+        )
+
+    def gen_mission(self):
+        if self._rand_float(0, 1) < self.locked_room_prob:
+            self.add_locked_room()
+
+        self.connect_all()
+
+        self.add_distractors(num_distractors=self.num_dists, all_unique=False)
+
+        # The agent must be placed after all the object to respect constraints
+        while True:
+            self.place_agent()
+            start_room = self.room_from_pos(*self.agent_pos)
+            # Ensure that we are not placing the agent in the locked room
+            if start_room is self.locked_room:
+                continue
+            break
+
+        # If no unblocking required, make sure all objects are
+        # reachable without unblocking
+        if not self.unblocking:
+            self.check_objs_reachable()
+
+        # Generate random instructions
+        self.instrs = self.rand_instr(
+            action_kinds=self.action_kinds, instr_kinds=self.instr_kinds
+        )
+
+    def add_locked_room(self):
+        # Until we've successfully added a locked room
+        while True:
+            i = self._rand_int(0, self.num_cols)
+            j = self._rand_int(0, self.num_rows)
+            door_idx = self._rand_int(0, 4)
+            self.locked_room = self.get_room(i, j)
+
+            # Don't add a locked door in an external wall
+            if self.locked_room.neighbors[door_idx] is None:
+                continue
+
+            door, _ = self.add_door(i, j, door_idx, locked=True)
+
+            # Done adding locked room
+            break
+
+        # Until we find a room to put the key
+        while True:
+            i = self._rand_int(0, self.num_cols)
+            j = self._rand_int(0, self.num_rows)
+            key_room = self.get_room(i, j)
+
+            if key_room is self.locked_room:
+                continue
+
+            self.add_object(i, j, "key", door.color)
+            break
+
+    def rand_obj(self, types=OBJ_TYPES, colors=COLOR_NAMES, max_tries=100):
+        """
+        Generate a random object descriptor
+        """
+
+        num_tries = 0
+
+        # Keep trying until we find a matching object
+        while True:
+            if num_tries > max_tries:
+                raise RecursionError("failed to find suitable object")
+            num_tries += 1
+
+            color = self._rand_elem([None, *colors])
+            type = self._rand_elem(types)
+
+            loc = None
+            if self.locations and self._rand_bool():
+                loc = self._rand_elem(LOC_NAMES)
+
+            desc = ObjDesc(type, color, loc)
+
+            # Find all objects matching the descriptor
+            objs, poss = desc.find_matching_objs(self)
+
+            # The description must match at least one object
+            if len(objs) == 0:
+                continue
+
+            # If no implicit unlocking is required
+            if not self.implicit_unlock and isinstance(self.locked_room, Room):
+                locked_room = self.locked_room
+                # Check that at least one object is not in the locked room
+                pos_not_locked = list(
+                    filter(lambda p: not locked_room.pos_inside(*p), poss)
+                )
+
+                if len(pos_not_locked) == 0:
+                    continue
+
+            # Found a valid object description
+            return desc
+
+    def rand_instr(self, action_kinds, instr_kinds, depth=0):
+        """
+        Generate random instructions
+        """
+
+        kind = self._rand_elem(instr_kinds)
+
+        if kind == "action":
+            action = self._rand_elem(action_kinds)
+
+            if action == "goto":
+                return GoToInstr(self.rand_obj())
+            elif action == "pickup":
+                return PickupInstr(self.rand_obj(types=OBJ_TYPES_NOT_DOOR))
+            elif action == "open":
+                return OpenInstr(self.rand_obj(types=["door"]))
+            elif action == "putnext":
+                return PutNextInstr(
+                    self.rand_obj(types=OBJ_TYPES_NOT_DOOR), self.rand_obj()
+                )
+
+            assert False
+
+        elif kind == "and":
+            instr_a = self.rand_instr(
+                action_kinds=action_kinds, instr_kinds=["action"], depth=depth + 1
+            )
+            instr_b = self.rand_instr(
+                action_kinds=action_kinds, instr_kinds=["action"], depth=depth + 1
+            )
+            return AndInstr(instr_a, instr_b)
+
+        elif kind == "seq":
+            instr_a = self.rand_instr(
+                action_kinds=action_kinds,
+                instr_kinds=["action", "and"],
+                depth=depth + 1,
+            )
+            instr_b = self.rand_instr(
+                action_kinds=action_kinds,
+                instr_kinds=["action", "and"],
+                depth=depth + 1,
+            )
+
+            kind = self._rand_elem(["before", "after"])
+
+            if kind == "before":
+                return BeforeInstr(instr_a, instr_b)
+            elif kind == "after":
+                return AfterInstr(instr_a, instr_b)
+
+            assert False
+
+        assert False
---
+++ minigrid/envs/babyai/other.py
@@ -0,0 +1,167 @@
+"""
+Copied and adapted from https://github.com/mila-iqia/babyai.
+Levels described in the Baby AI ICLR 2019 submission, with different instructions than those in other files.
+"""
+
+from minigrid.envs.babyai.core.roomgrid_level import RoomGridLevel
+from minigrid.envs.babyai.core.verifier import (
+    BeforeInstr,
+    GoToInstr,
+    ObjDesc,
+    OpenInstr,
+    PickupInstr,
+    PutNextInstr,
+)
+
+
+class ActionObjDoor(RoomGridLevel):
+    """
+    [pick up an object] or
+    [go to an object or door] or
+    [open a door]
+    (in the current room)
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(room_size=7, **kwargs)
+
+    def gen_mission(self):
+        objs = self.add_distractors(1, 1, num_distractors=5)
+        for _ in range(4):
+            door, _ = self.add_door(1, 1, locked=False)
+            objs.append(door)
+
+        self.place_agent(1, 1)
+
+        obj = self._rand_elem(objs)
+        desc = ObjDesc(obj.type, obj.color)
+
+        if obj.type == "door":
+            if self._rand_bool():
+                self.instrs = GoToInstr(desc)
+            else:
+                self.instrs = OpenInstr(desc)
+        else:
+            if self._rand_bool():
+                self.instrs = GoToInstr(desc)
+            else:
+                self.instrs = PickupInstr(desc)
+
+
+class FindObjS5(RoomGridLevel):
+    """
+    Pick up an object (in a random room)
+    Rooms have a size of 5
+    This level requires potentially exhaustive exploration
+    """
+
+    def __init__(self, room_size=5, **kwargs):
+        super().__init__(room_size=room_size, max_steps=20 * room_size**2, **kwargs)
+
+    def gen_mission(self):
+        # Add a random object to a random room
+        i = self._rand_int(0, self.num_rows)
+        j = self._rand_int(0, self.num_cols)
+        obj, _ = self.add_object(i, j)
+        self.place_agent(1, 1)
+        self.connect_all()
+
+        self.instrs = PickupInstr(ObjDesc(obj.type))
+
+
+class KeyCorridor(RoomGridLevel):
+    """
+    A ball is behind a locked door, the key is placed in a
+    random room.
+    """
+
+    def __init__(self, num_rows=3, obj_type="ball", room_size=6, **kwargs):
+        self.obj_type = obj_type
+
+        super().__init__(
+            room_size=room_size,
+            num_rows=num_rows,
+            max_steps=30 * room_size**2,
+            **kwargs
+        )
+
+    def gen_mission(self):
+        # Connect the middle column rooms into a hallway
+        for j in range(1, self.num_rows):
+            self.remove_wall(1, j, 3)
+
+        # Add a locked door on the bottom right
+        # Add an object behind the locked door
+        room_idx = self._rand_int(0, self.num_rows)
+        door, _ = self.add_door(2, room_idx, 2, locked=True)
+        obj, _ = self.add_object(2, room_idx, kind=self.obj_type)
+
+        # Add a key in a random room on the left side
+        self.add_object(0, self._rand_int(0, self.num_rows), "key", door.color)
+
+        # Place the agent in the middle
+        self.place_agent(1, self.num_rows // 2)
+
+        # Make sure all rooms are accessible
+        self.connect_all()
+
+        self.instrs = PickupInstr(ObjDesc(obj.type))
+
+
+class OneRoomS8(RoomGridLevel):
+    """
+    Pick up the ball
+    Rooms have a size of 8
+    """
+
+    def __init__(self, room_size=8, **kwargs):
+        super().__init__(room_size=room_size, num_rows=1, num_cols=1, **kwargs)
+
+    def gen_mission(self):
+        obj, _ = self.add_object(0, 0, kind="ball")
+        self.place_agent()
+        self.instrs = PickupInstr(ObjDesc(obj.type))
+
+
+class MoveTwoAcross(RoomGridLevel):
+    """
+    Task of the form: move the A next to the B and the C next to the D.
+    This task is structured to have a very large number of possible
+    instructions.
+    """
+
+    def __init__(self, room_size, objs_per_room, **kwargs):
+        assert objs_per_room <= 9
+        self.objs_per_room = objs_per_room
+
+        super().__init__(
+            num_rows=1,
+            num_cols=2,
+            room_size=room_size,
+            max_steps=16 * room_size**2,
+            **kwargs
+        )
+
+    def gen_mission(self):
+        self.place_agent(0, 0)
+
+        # Add objects to both the left and right rooms
+        # so that we know that we have two non-adjacent set of objects
+        objs_l = self.add_distractors(0, 0, self.objs_per_room)
+        objs_r = self.add_distractors(1, 0, self.objs_per_room)
+
+        # Remove the wall between the two rooms
+        self.remove_wall(0, 0, 0)
+
+        # Select objects from both subsets
+        objs_l = self._rand_subset(objs_l, 2)
+        objs_r = self._rand_subset(objs_r, 2)
+        a = objs_l[0]
+        b = objs_r[0]
+        c = objs_r[1]
+        d = objs_l[1]
+
+        self.instrs = BeforeInstr(
+            PutNextInstr(ObjDesc(a.type, a.color), ObjDesc(b.type, b.color)),
+            PutNextInstr(ObjDesc(c.type, c.color), ObjDesc(d.type, d.color)),
+        )
