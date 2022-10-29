2fa99b2de6775b2b809abaab63d2350fbcfbf2cb
Adhocmaster <adhocmaster@live.com>
2022-10-29 10:43:36 -0700

    refactored

--- gym_minigrid/envs/pedestrian/__init__.py
+++ gym_minigrid/envs/pedestrian/__init__.py
@@ -1,3 +1,3 @@
-print("importing pedestrian classes")
+from gym_minigrid.envs.pedestrian.PedGrid import *
 from gym_minigrid.envs.pedestrian.pedestrian import *
 from gym_minigrid.envs.pedestrian.MultiPedestrianEnv import *
--- test-multi-pedestrian.py
+++ test-multi-pedestrian.py
@@ -3,6 +3,7 @@ import random
 import gym
 import gym_minigrid
 from gym_minigrid.wrappers import *
+from gym_minigrid.agents import PedAgent
 # from gym_minigrid.envs import MultiPedestrianEnv
 # %matplotlib auto
 # %load_ext autoreload
@@ -10,7 +11,8 @@ from gym_minigrid.wrappers import *
 
 # Load the gym environment
 env = gym.make('MultiPedestrian-Empty-20x80-v0')
-env = FullyObsWrapper(env)
+env.addAgents([PedAgent((5, 5), 3, 2), PedAgent((7, 10), 3, 2), PedAgent((2, 5), 3, 2)])
+# env = FullyObsWrapper(env)
 # env = RGBImgPartialObsWrapper(env) # Get pixel observations
 # env = ImgObsWrapper(env) # Get rid of the 'mission' field
 env.reset()
@@ -18,13 +20,8 @@ env.reset()
 for i in range(0, 20):
 #     print("step {}".format(i))
 
-    # Pick a random action
-#     action = random.randint(0, env.action_space.n - 1)
-    actions = []
-    for j in range(0, 3):
-        actions.append(random.randint(0, 2))
 
-    obs, reward, done, info = env.step(actions)
+    obs, reward, done, info = env.step(None)
     # print(obs)
 #     print(obs["direction"])
 #     print(obs["position"])
