86f1574a676d7334391a79fc199aaf6fea9b8e35
Zarif Ikram <65703522+zarifikram@users.noreply.github.com>
2022-11-10 14:10:46 +0600

    fixed the environment

--- test-multi-pedestrian.py
+++ test-multi-pedestrian.py
@@ -12,9 +12,7 @@ from gym_minigrid.agents import PedAgent
 
 # Load the gym environment
 env = gym.make('MultiPedestrian-Empty-20x80-v0')
-<<<<<<< Updated upstream
 env.addAgents([PedAgent((5, 5), 3, 2), PedAgent((7, 10), 3, 2), PedAgent((2, 5), 3, 2)])
-=======
 agents = []
 for i in range(50):
     xPos = np.random.randint(2, 18)
@@ -30,19 +28,11 @@ for i in range(50):
     agents.append(PedAgent((xPos, yPos), direction, speed))
 env.addAgents(agents)
 
->>>>>>> Stashed changes
-# env = FullyObsWrapper(env)
-# env = RGBImgPartialObsWrapper(env) # Get pixel observations
-# env = ImgObsWrapper(env) # Get rid of the 'mission' field
 env.reset()
 
-for i in range(0, 400):
-#     print("step {}".format(i))
+for i in range(0, 100):
 
     obs, reward, done, info = env.step(None)
-    # print(obs)
-#     print(obs["direction"])
-#     print(obs["position"])
     
     if done:
         "Reached the goal"
@@ -52,7 +42,6 @@ for i in range(0, 400):
 
     time.sleep(0.05)
 
-#     time.sleep(0.05)
 
 # Test the close method
 env.close()
