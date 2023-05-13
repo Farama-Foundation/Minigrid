f4b2a64ecfc6e5f7605e3a2d3ad47c5b03e7693f
Taorui Huang <taoruih@gmail.com>
2023-05-13 14:28:07 -0700

    Updated Road.py & new Env

--- gym_minigrid/envs/pedestrian/TwoLaneRoadEnv.py
+++ gym_minigrid/envs/pedestrian/TwoLaneRoadEnv.py
@@ -1,12 +1,39 @@
 from .PedestrianEnv import PedestrianEnv
+
+from typing import List
+from gym_minigrid.minigrid import *
 from gym_minigrid.register import register
+from gym_minigrid.agents import Agent, PedActions, PedAgent, Road, Lane
+from gym_minigrid.envs.pedestrian.PedGrid import PedGrid
+from gym_minigrid.lib.Action import Action
+from gym_minigrid.lib.LaneAction import LaneAction
+from gym_minigrid.lib.ForwardAction import ForwardAction
+from gym_minigrid.lib.Direction import Direction
+from .EnvEvent import EnvEvent
+import logging
+import random
 
 class TwoLaneRoadEnv(PedestrianEnv):
     # Write the outline here how it should work
 
     # generic object representation
     # generic actors?
-    
+    def __init__(
+        self,
+        agents: List[Agent]=None,
+        roads: List[Road]=None,
+        width=8,
+        height=8,
+        stepsIgnore = 100
+    ):
+        
+        super().__init__(
+            agents=agents,
+            width=width,
+            height=height,
+            stepsIgnore=stepsIgnore
+        )
+        
 
     pass
 
