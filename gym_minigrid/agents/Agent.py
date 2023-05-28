03fddac90b78dcd28d5e048852275df28b6d7297
Taorui Huang <taoruih@gmail.com>
2023-05-27 17:04:18 -0700

    Update Agent.py

--- gym_minigrid/agents/Agent.py
+++ gym_minigrid/agents/Agent.py
@@ -12,7 +12,7 @@ class Agent(BaseObject):
         direction: int, # TODO convert direction to enum
         maxSpeed: float = 4,
         speed: float = 3,
-        objectType=None
+        objectType="Agent"
     ):
 
         self.id = id
