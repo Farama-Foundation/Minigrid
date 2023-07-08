74d0bf1f0883423b620d0ba076a539690ac0c626
James Doran <jysdoran@gmail.com>
2023-07-08 10:43:46 +0100

    Rename wfcroom.py because it's not a room like multiroom

--- minigrid/envs/wfc/__init__.py
+++ minigrid/envs/wfc/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
 from minigrid.envs.wfc.config import WFC_PRESETS, WFC_PRESETS_SLOW, WFCConfig
-from minigrid.envs.wfc.wfcroom import WFCEnv
+from minigrid.envs.wfc.wfcenv import WFCEnv
