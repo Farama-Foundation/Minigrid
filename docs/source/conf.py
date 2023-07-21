2922ff5b8f85d614744f13dfa94b5ea4e35e0408
adhocmaster <adhocmaster@live.com>
2023-07-21 10:38:14 -0700

    first version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,65 @@ author = 'Golam Md Muktadir, Taorui Huang, Zarif Ikram, Abdul Jawad'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autosectionlabel',
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.coverage', 
+    'sphinx.ext.napoleon'
+    ]
+autosectionlabel_prefix_document = True
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+# html_css_files = [
+#     'css/custom.css',
+# ]
+html_style = 'css/custom.css'
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "tasklist",
+]
+
+
+
+language = "en"
+myst_html_meta = {
+    "description lang=en": "Procedural Generation of Intersections and HD Maps for Autonomous Vehicle Development and Test.",
+    "keywords": "JunctionArt, Road Generator, HD Map generator OpenDrive, Opendrive road generator, Carla HD Maps, Autonomous Vehicles Map Generator",
+    "property=og:locale":  "en_US"
+}
