f69415506e120c96894db70b2631356b13154319
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2024-01-15 08:53:25 -0500

    updated class name for obstructedmaze_v1

--- docs/_scripts/gen_envs_display.py
+++ docs/_scripts/gen_envs_display.py
@@ -77,19 +77,9 @@ if __name__ == "__main__":
         env_type = key
 
         page = generate_page({"id": key, "list": value})
-        # fp = open(
-        #     os.path.join(
-        #         os.path.dirname(__file__), "..", "environments", env_type, "list.html"
-        #     ),
-        #     "w",
-        #     encoding="utf-8",
-        # )
         fp = open(
             os.path.join(
-                os.path.dirname(__file__),
-                "..",
-                "environments/minigrid",
-                f"{env_type}.md",
+                os.path.dirname(__file__), "..", "environments", env_type, "list.html"
             ),
             "w",
             encoding="utf-8",
