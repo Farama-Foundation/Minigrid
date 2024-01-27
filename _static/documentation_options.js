5be108b5bac22fb1284d4058e96d41ff7d3b8526
BolunDai0216 <36321182+BolunDai0216@users.noreply.github.com>
2024-01-27 16:20:21 +0000

    Deploying to gh-pages from @ Farama-Foundation/Minigrid@f3869ebc021b57cd14418ac68095ebe540ff75ff 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.0',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.3.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
