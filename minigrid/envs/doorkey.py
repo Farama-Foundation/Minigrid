9b8f7b7c9ca28f2ec19a0040e64dd4d3de13f3da
Rafael Bankosegger <rafael@bankosegger.at>
2024-04-22 15:09:01 +0200

    Fix issue when generating non-square DoorKey environments (#427)

--- minigrid/envs/doorkey.py
+++ minigrid/envs/doorkey.py
@@ -91,7 +91,7 @@ class DoorKeyEnv(MiniGridEnv):
         self.place_agent(size=(splitIdx, height))
 
         # Place a door in the wall
-        doorIdx = self._rand_int(1, width - 2)
+        doorIdx = self._rand_int(1, height - 2)
         self.put_obj(Door("yellow", is_locked=True), splitIdx, doorIdx)
 
         # Place a yellow key on the left side
