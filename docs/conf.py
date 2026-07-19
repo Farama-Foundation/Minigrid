bfdad3877bc5d266e897fdbb3672f760120cf7ea
Hugo <hugo@hugohuang.com>
2026-07-19 04:53:24 +0100

    Change HTML theme from 'furo' to 'celshast'

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ napoleon_custom_sections = [("Returns", "params_style")]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "celshast"
 html_title = "MiniGrid Documentation"
 html_baseurl = "https://minigrid.farama.org/"
 html_copy_source = False
